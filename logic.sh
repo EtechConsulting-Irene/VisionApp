7ad8b19234e9ade235629390f06fa6a934216d83
irne11 <egonwecknaomi.2010@gmail.com>
2024-10-27 12:46:15 +0100

    Uat (#16)
    * new script added to the project
    
    * Alex 05 11 (#11) (#12)
    
    * testing branch protection rules
    
    * git is fun
    
    * Dev (#15)
    
    * Alex 05 11 (#11)
    
    * testing branch protection rules
    
    * git is fun
    
    * doccuments  (#14)
    
    * new script added to the project (#9)
    
    * Uat (#13)
    
    * new script added to the project
    
    * Alex 05 11 (#11) (#12)
    
    * testing branch protection rules
    
    * git is fun
    
    * Update README.md
    
    * added new file
    
    * visual studio demo

--- logic.sh
+++ logic.sh
@@ -1,4 +1,4 @@
-exit status error codes and their exit values
+
 echo $? = 0 means the last run command was successful
 echo $? = 1  cat: doesnotexist.txt: No such file or directory
 echo $? = 1 - Catchall for general errors
@@ -9,4 +9,4 @@ echo $? = 128+n - Fatal error signal “n”
 echo $? = 130 - Script terminated by Control-C
 echo $? = 255\* - Exit status out of range
 #this was just for testing
-# Git is easy
+
